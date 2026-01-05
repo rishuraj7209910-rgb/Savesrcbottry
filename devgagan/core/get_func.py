ae93f54276c7ae3c943c616da4ef97eaf1450c58
rishuraj7209910-rgb <rishu.raj7209910@gmail.com>
2026-01-05 09:28:45 +0530

    Update watermark contact in get_func.py

--- devgagan/core/get_func.py
+++ devgagan/core/get_func.py
@@ -667,7 +667,7 @@ async def callback_query_handler(event):
         await event.respond('Please send the photo you want to set as the thumbnail.')
     
     elif event.data == b'pdfwt':
-        await event.respond("Watermark is Pro+ Plan.. contact @kingofpatal")
+        await event.respond("Watermark is Pro+ Plan.. contact @RishuBotz_Bot")
         return
 
     elif event.data == b'uploadmethod':
